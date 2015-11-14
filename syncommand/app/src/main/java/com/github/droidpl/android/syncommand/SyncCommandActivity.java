d3dc3627dbaabb0e13c345ecf2a05a5ad46e2ce2
Javier de Pedro López <javierdepedrolopez@gmail.com>
2015-11-14 15:08:19 +0100

    Added the api

--- syncommand/app/src/main/java/com/github/droidpl/android/syncommand/SyncCommandActivity.java
+++ syncommand/app/src/main/java/com/github/droidpl/android/syncommand/SyncCommandActivity.java
@@ -1,13 +1,58 @@
 package com.github.droidpl.android.syncommand;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class SyncCommandActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.nearby.Nearby;
+
+public class SyncCommandActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+
+    private GoogleApiClient mClient;
+    private boolean mIsConnected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sync_command);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mClient = new GoogleApiClient.Builder(this)
+                .addApi(Nearby.MESSAGES_API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mClient != null){
+            mClient.disconnect();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
