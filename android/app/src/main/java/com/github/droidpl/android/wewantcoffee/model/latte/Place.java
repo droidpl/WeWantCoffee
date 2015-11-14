bbef959ccdddfca83daa87faca049d7f691b830e
Steven <sluijts@gmail.com>
2015-11-14 14:14:53 +0100

    Added models for Latte Locator

---
+++ android/app/src/main/java/com/github/droidpl/android/wewantcoffee/model/latte/Place.java
@@ -0,0 +1,13 @@
+package com.github.droidpl.android.wewantcoffee.model.latte;
+
+/**
+ * Created by steven on 14/11/15.
+ */
+public class Place {
+
+    public String name;
+    public int price_level;
+    public float rating;
+    public String vicinity;
+
+}
---
+++ android/app/src/main/java/com/github/droidpl/android/wewantcoffee/model/latte/PlaceResult.java
@@ -0,0 +1,10 @@
+package com.github.droidpl.android.wewantcoffee.model.latte;
+
+/**
+ * Created by steven on 14/11/15.
+ */
+public class PlaceResult {
+
+    public Place[] results;
+
+}
