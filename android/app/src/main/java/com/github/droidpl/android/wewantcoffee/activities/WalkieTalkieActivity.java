97eea6a1add1989447c06c90d9f3f0ab3e09b972
Bryan te Beek <bryantebeek@gmail.com>
2015-11-14 13:18:29 +0100

    Add WalkieTalkie activity to be launched from the widget

--- android/app/src/main/java/com/github/droidpl/android/wewantcoffee/activities/WalkieTalkieActivity.java
+++ android/app/src/main/java/com/github/droidpl/android/wewantcoffee/activities/WalkieTalkieActivity.java
@@ -7,7 +7,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.droidpl.android.wewantcoffee.R;
@@ -16,6 +19,7 @@ import java.util.ArrayList;
 
 public class WalkieTalkieActivity extends AppCompatActivity {
 
+    private String phone;
     private static final int REQUEST_SPEECH = 0;
 
     @Override
@@ -29,6 +33,8 @@ public class WalkieTalkieActivity extends AppCompatActivity {
         i.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 5);
         i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
         startActivityForResult(i, REQUEST_SPEECH);
+
+        phone = getIntent().getStringExtra("phone");
     }
 
     @Override
@@ -36,7 +42,21 @@ public class WalkieTalkieActivity extends AppCompatActivity {
         switch(requestCode) {
             case REQUEST_SPEECH:
                 ArrayList results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                Toast.makeText(this, (String) results.get(0), Toast.LENGTH_LONG).show();
+
+                final String speech = (String) results.get(0);
+                TextView tvSpeech = (TextView) findViewById(R.id.tvSpeech);
+                tvSpeech.setText(speech);
+
+                Button btnSendSpeech = (Button) findViewById(R.id.btnSendSpeech);
+                btnSendSpeech.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (phone != null) {
+                            SmsManager.getDefault().sendTextMessage(phone, null, speech, null, null);
+                        }
+                    }
+                });
+
                 break;
         }
     }
